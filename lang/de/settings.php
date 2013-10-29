308a8adb3f836a4e56b7e6e508e2cde4b199ae98
Lars Pötter <Lars_poetter@gmx.de>
2013-10-29 16:20:09 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,23 +1,12 @@
 <?php
-/*
- * Copyright (c) 2013 Mark C. Prins <mprins@users.sf.net>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 
 /**
- * language file for socialcards plugin.
- *
- * @license BSD license
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author Lars Pötter <Lars_poetter@gmx.de>
  */
+$lang['twitterName']           = 'Twitter Account Name für diese Seite';
+$lang['fallbackImage']         = 'Standard(ersatz) Bild für OpenGraph und Twitter cards.(eine URL oder Media ID)';
+$lang['languageTerritory']     = 'Locale(Sprache und Land)';
+$lang['fbAppId']               = 'Facebook App ID (optional)';
