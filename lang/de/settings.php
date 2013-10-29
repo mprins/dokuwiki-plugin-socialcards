4c64c2300d89f4349a226ee9cded4c718edd09f0
Mark Prins <mprins@users.sf.net>
2013-10-29 22:51:44 +0100

    Update settings.php
    cherrypick proposed changes of #9

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -20,4 +20,9 @@
  *
  * @license BSD license
  * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author Lars Pötter <Lars_poetter@gmx.de>
  */
+$lang['twitterName']           = 'Twitter Account Name für diese Seite';
+$lang['fallbackImage']         = 'Standard(ersatz) Bild für OpenGraph und Twitter cards.(eine URL oder Media ID)';
+$lang['languageTerritory']     = 'Locale(Sprache und Land)';
+$lang['fbAppId']               = 'Facebook App ID (optional)';
