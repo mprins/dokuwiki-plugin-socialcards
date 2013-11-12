c1734ae4af3151c3f703a3d2792ebc2952e4a536
Mark Prins <mprins@users.sf.net>
2013-11-12 17:13:09 +0100

    Update settings.php

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -21,8 +21,9 @@
  * @license BSD license
  * @author  Mark C. Prins <mprins@users.sf.net>
  * @author  Myeongjin <aranet100@gmail.com>
+ * 
  */
-$lang['twitterName'] = '이 사이트에 대한 트위터 계정 이름.';
-$lang['fallbackImage'] = '오픈그래프와 트위터 카드에 대한 기본 (폴백) 그림. (정규화된 url 또는 미디어 id)';
+$lang['twitterName']       = '이 사이트에 대한 트위터 계정 이름.';
+$lang['fallbackImage']     = '오픈그래프와 트위터 카드에 대한 기본 (폴백) 그림. (정규화된 url 또는 미디어 id)';
 $lang['languageTerritory'] = '로캘. (언어와 지역)';
-$lang['fbAppId'] = '페이스북 앱 ID (선택 사항)';
+$lang['fbAppId']           = '페이스북 앱 ID (선택 사항)';
