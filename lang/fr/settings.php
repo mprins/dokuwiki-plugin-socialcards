dc60aa7629bcd03d01e466c77df1a6e83cb003fa
Nicolas Friedli <nicolas@theologique.ch>
2013-12-16 21:00:04 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,23 +1,12 @@
 <?php
-/*
- * Copyright (c) 2013 Mark C. Prins <mprins@users.sf.net>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 
 /**
- * language file for socialcards plugin.
- *
- * @license BSD license
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
+$lang['twitterName']           = 'Nom du compte twitter de ce site.';
+$lang['fallbackImage']         = 'Image par défaut pour OpenGraph ou Twitter cards. (une URL absolue ou un id de média)';
+$lang['languageTerritory']     = 'Locale (langue et pays).';
+$lang['fbAppId']               = 'ID de l\'application Facebook (optionnel).';
