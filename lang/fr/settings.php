6c8d36e16baccef62e745f234d9f751c30267b95
Mark Prins <mprins@users.sf.net>
2013-12-18 11:56:27 +0100

    Update settings.php
    adding French translation contributed in #11

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -20,4 +20,9 @@
  *
  * @license BSD license
  * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
+$lang['twitterName']           = 'Nom du compte twitter de ce site.';
+$lang['fallbackImage']         = 'Image par défaut pour OpenGraph ou Twitter cards. (une URL absolue ou un id de média)';
+$lang['languageTerritory']     = 'Locale (langue et pays).';
+$lang['fbAppId']               = 'ID de l\'application Facebook (optionnel).';
