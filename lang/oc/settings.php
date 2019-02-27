ed2b255968eac05849f6288543a906a8d307a68b
En Matt <heartattack@free.fr>
2019-02-27 20:55:26 +0100

    translation update

---
+++ lang/oc/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author En Matt <heartattack@free.fr>
+ */
+$lang['twitterName']           = 'Nom d’utilizaire Twitter per aqueste site';
+$lang['twitterUserName']       = 'Nom d’utilizaire Twitter de l’autor';
+$lang['fallbackImage']         = 'Imatge per defaut (se per cas) per OpenGraph e las cartas Twitter. (una URL de qualitat o un id de mèdia)';
+$lang['languageTerritory']     = 'Lenga (Lenga e país)';
+$lang['fbAppId']               = 'Facebook App ID (opcional)';
