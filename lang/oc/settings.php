c07a5eee990600395cfda72496680989759132dc
DokuWiki Translation Tool <admin@dokuwiki.org>
2019-02-28 09:36:08 +0100

    Translation update (oc) (#24)

---
+++ lang/oc/settings.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright (c) 2019 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * language file for socialcards plugin.
+ *
+ * @license BSD license
+ * @author  Mark C. Prins <mprins@users.sf.net>
+ * @author  En Matt <heartattack@free.fr>
+ */
+$lang['twitterName']           = 'Nom d’utilizaire Twitter per aqueste site';
+$lang['twitterUserName']       = 'Nom d’utilizaire Twitter de l’autor';
+$lang['fallbackImage']         = 'Imatge per defaut (se per cas) per OpenGraph e las cartas Twitter. (una URL de qualitat o un id de mèdia)';
+$lang['languageTerritory']     = 'Lenga (Lenga e país)';
+$lang['fbAppId']               = 'Facebook App ID (opcional)';
